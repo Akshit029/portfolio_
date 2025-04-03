8dd61038456f6ba6710966ea709346c8af5eb73f
Akshit chadgal <akshitchadgal016@gmail.com>
2025-04-03 12:55:38 +0530

    .

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,9 @@
-import { RiNextjsFill, RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiMongodb } from "react-icons/si";
-import { DiRedis } from "react-icons/di";
+import {  RiReactjsLine } from "react-icons/ri";  //RiNextjsFill,
+// import { TbBrandNextjs } from "react-icons/tb";
+import { SiMongodb , SiTailwindcss } from "react-icons/si";
+// import { DiRedis } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa";
-import { BiLogoPostgresql } from "react-icons/bi";
+// import { BiLogoPostgresql } from "react-icons/bi";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -39,13 +39,23 @@ const Technologies = () => {
          className="rounded-2xl border-4 border-neutral-800 p-4">
             <RiReactjsLine  className="text-7xl text-cyan-400"/>
         </motion.div>
+        
         <motion.div 
+         variants={iconVariants(2.5)}
+         initial="inital"
+         animate="animate"
+         className="rounded-2xl border-4 border-neutral-800 p-4">
+            <SiTailwindcss  className="text-7xl text-cyan-400"/>
+        </motion.div>
+
+        {/* <motion.div 
          variants={iconVariants(3)}
          initial="inital"
          animate="animate"
          className="rounded-2xl border-4 border-neutral-800 p-4">
             <TbBrandNextjs  className="text-7xl"/>
-        </motion.div>
+        </motion.div> */}
+
         <motion.div
          variants={iconVariants(5)}
          initial="inital"
@@ -53,13 +63,15 @@ const Technologies = () => {
          className="rounded-2xl border-4 border-neutral-800 p-4">
             <SiMongodb  className="text-7xl text-green-500"/>
         </motion.div>
-        <motion.div
+
+        {/* <motion.div
          variants={iconVariants(2)}
          initial="inital"
          animate="animate"
          className="rounded-2xl border-4 border-neutral-800 p-4">
             <DiRedis className="text-7xl text-red-700"/>
-        </motion.div>
+        </motion.div> */}
+
         <motion.div
          variants={iconVariants(6)}
          initial="inital"
@@ -67,13 +79,15 @@ const Technologies = () => {
          className="rounded-2xl border-4 border-neutral-800 p-4">
             <FaNodeJs  className="text-7xl text-green-500"/>
         </motion.div>
-        <motion.div 
+
+        {/* <motion.div 
         variants={iconVariants(4)}
          initial="inital"
          animate="animate"
          className="rounded-2xl border-4 border-neutral-800 p-4">
             <BiLogoPostgresql  className="text-7xl text-sky-700"/>
-        </motion.div>
+        </motion.div> */}
+
       </motion.div>
     </div>
   )
